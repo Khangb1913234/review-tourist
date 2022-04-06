6b81b00a7571a0fbe3a9bd2df176a872e0ea33a3
DangVuDinhKhang <khangb1913234@student.ctu.edu.vn>
2022-04-06 19:37:22 +0700

    Sua cac loi con lai

--- app/routes/review.route.js
+++ app/routes/review.route.js
@@ -1,13 +1,13 @@
 const express = require("express");
-const contacts = require("../controllers/review.controller");
+const reviews = require("../controllers/review.controller");
 module.exports = function(app){
     const router = express.Router();
-    router.post("/", contacts.create);
-    router.get("/", contacts.findAll);
-    router.get("/favorite", contacts.findAllFavorite);
-    router.get("/:id", contacts.findOne);
-    router.put("/:id", contacts.update);
-    router.delete("/:id", contacts.delete);
-    router.delete("/", contacts.deleteAll);
+    router.post("/", reviews.create);
+    router.get("/", reviews.findAll);
+    router.get("/favorite", reviews.findAllFavorite);
+    router.get("/:id", reviews.findOne);
+    router.put("/:id", reviews.update);
+    router.delete("/:id", reviews.delete);
+    router.delete("/", reviews.deleteAll);
     app.use("/api/review", router);
 };
