4f1fa15fd9f5e73e0c3a9b8439419ea1ea096bb6
DangVuDinhKhang <khangb1913234@student.ctu.edu.vn>
2022-04-06 19:35:28 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { BadRequestError, errorHandler } = require("./app/errors");
+const { BadRequestError, errorHandler } = require("./app/error");
 
 const app = express();
 
@@ -12,18 +12,18 @@ app.use(express.urlencoded({ extended: true }));
 const setupContactRoutes = require("./app/routes/review.route");
 setupContactRoutes(app);
 
-// app.use((req, res, next) => {
-//     // Code ở đây sẽ chạy khi không có route được định nghĩa nào
-//     // khớp với yêu cầu. Gọi next() để chuyển sang middleware xử lý lỗi
-//     next(new BadRequestError(404, "Resource not found"));
-// });
+app.use((req, res, next) => {
+    // Code ở đây sẽ chạy khi không có route được định nghĩa nào
+    // khớp với yêu cầu. Gọi next() để chuyển sang middleware xử lý lỗi
+    next(new BadRequestError(404, "Không tìm thấy tài nguyên"));
+});
 
-// app.use((err, req, res, next) => {
-//     // Middleware xử lý lỗi tập trung.
-//     // Trong các đoạn code xử lý ở các route, gọi next(error)
-//     // sẽ chuyển về middleware xử lý lỗi này
-//     errorHandler.handleError(err, res);
-// });
+app.use((err, req, res, next) => {
+    // Middleware xử lý lỗi tập trung.
+    // Trong các đoạn code xử lý ở các route, gọi next(error)
+    // sẽ chuyển về middleware xử lý lỗi này
+    errorHandler.handleError(err, res);
+});
 
 app.get('/', function(req, res){
     res.json({ message: 'Chào mừng đến với app giới thiệu địa điểm du lịch' });
